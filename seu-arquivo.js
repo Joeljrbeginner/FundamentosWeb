97e8fe25d36a6b5936205023a6bd1cc2b038bd8c
Joeljrbeginner <joeljuniormooca364@gmail.com>
2023-03-11 22:51:00 -0300

    novo arquivo.js

---
+++ seu-arquivo.js
@@ -0,0 +1,5 @@
+// cria uma variável para armazenar a parte da letra da música
+var myWayLyrics = "And now, the end is near\nAnd so I face the final curtain\nMy friend, I'll say it clear\nI'll state my case, of which I'm certain";
+
+// exibe a letra na console do navegador
+console.log(myWayLyrics);
